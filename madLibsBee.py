c9319dedcbd65d856e107bb555c212f6c181f35a
Nicholas Price <nprice14@terpmail.umd.edu>
2025-04-08 23:15:48 -0400

    first commit

--- madLibsBee.py
+++ madLibsBee.py
@@ -3,8 +3,12 @@
 
 # vsayasa Edited this @ 4/8/2025
 # Nell :)
+# Nico was here
 
 class Player:
     """ Single person player  """
     def init(self, player):
         self.player= player
+    
+def hint():
+    pass
